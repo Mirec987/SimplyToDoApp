340f7183bd30b356ec52208603ee3f4f6b674048
Miroslav Luzbetak <miroslavluzbetak@gmail.com>
2024-09-11 18:42:09 +0200

    Initial commit

---
+++ ToDoApp/Controllers/ToDoItemsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.Data;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoItemsController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoItemsController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItems()
+        {
+            return await _context.ToDoItems.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem item)
+        {
+            _context.ToDoItems.Add(item);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetToDoItems), new { id = item.Id }, item);
+        }
+    }
+}
---
+++ ToDoApp/Data/ToDoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) { }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
